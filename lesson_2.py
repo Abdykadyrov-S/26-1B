3807a448318855cbfc71a54f38fd25930103ce5f
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2025-01-23 13:44:51 +0600

    lesson 2

---
+++ lesson_2.py
@@ -0,0 +1,93 @@
+# input # команда для ввода
+# print # команда для вывода
+
+# print("Hello world")
+"Переменные" 
+
+# input("Введите имя: ") 
+# input("Укажите возраст: ")
+
+# print()
+
+# a = 5
+# print(a)
+# b = 7
+# print(7)
+
+# #    12
+# c = a + b
+# print(c) 
+# print(c + 10) 
+# print(a + b)
+# print(5 + 7)
+
+# print(a + 45)
+
+# 6 = "qwerty" ❌ 
+# 123456 = 123456 ❌
+# 5_number = 5
+# number5 = 5
+# number_5 = 5
+
+# тест = 123456 ❌
+# qwerty = 123456
+
+# qwerty = "Hello world" ❌
+# number = 12
+# name = "Сыймык"
+
+# my first name = "Сыймык"❌
+# my-first-name = "Сыймык"❌
+
+# myfirstname = "Сыймык"
+# my_first_name = "Сыймык"
+
+"snake case - змеиный регистр"
+# my_first_name = "Сыймык"
+
+
+"camel case - верблюжий регистр"
+# MyFirstName = "Сыймык"
+
+"верхний регистр"
+# NAME = "Сыймык"
+
+"нижний регистр"
+# name = "Сыймык"
+
+
+# name = input("Укажите имя: ")
+
+# print(name)
+
+
+
+
+
+
+# input("Здравствуйте: ") 
+# input("Введите имя: ")
+
+# name = input("Введите имя: ")
+# print("Hello", name)
+
+# a = input("Введите имя: ")
+# b = input("Введите фамилию: ")
+
+"""
+Запросит у пользователя 2 числа
+Выведите результат сложения этих двух чисел
+"""
+
+# num1 = input("Введите первое число: ")
+# num2 = input("Введите второе число: ")
+
+# print(num1 + num2)
+
+# num1 = 5 
+# num2 = 5 
+
+# print(num1)
+# print(num2)
+# print(num1 + num2)
+
